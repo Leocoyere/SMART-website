409fddc47728e89327dd92a7b9bbb16f0f639406
Leocoyere <72854281+Leocoyere@users.noreply.github.com>
2021-05-14 14:59:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 h3_list = [ "Penalty", "Validate", "Special", "Weakness"]
 
-p_list = [ "If you choose the wrong word, you make a grammar mistake, it makes you lose IQ. Same if you end a sentence in the wrong way.", "When you consider that your sentence is finished, you can validate by clicking in the button next to your IQ bar. It ends your turn. Then you tell your sentence to the opponent and he takes damages according to the words in your sentence.", "Each player has a special. It allow him to add a special word that suits to his sentence. Each player has his own special words. You can activate your special while clicking on your player. When you use your special you employ a lot of concentration, that makes you lose IQ. With great power comes great responsibility. Spend it wisely.", "Each player has his own weaknesses. If your sentence contain a word related to your opponent weakness, it will count twice."]
+p_list = [ "If you choose the wrong word, you make a grammar mistake, it makes you lose IQ. Same if you end a sentence in the wrong way.", "When you consider that your sentence is finished, you can validate by clicking in the button next to your IQ bar. It ends your turn. Then you tell your sentence to the opponent and he takes damages according to the words in your sentence.", "Each player has a special. It allows him to add a special word that suits to his sentence. Each player has his own special words. You can activate your special while clicking on your player. When you use your special you employ a lot of concentration, that makes you lose IQ. With great power comes great responsibility. Spend it wisely.", "Each player has his own weaknesses. If your sentence contains a word related to your opponent weakness, it will count twice."]
 
 document.querySelector(".page:nth-of-type(1)").addEventListener('click', () => {
     rules(".page", 1)
@@ -27,20 +27,10 @@ function rules(list, number) {
     document.querySelector("#rules > p").innerHTML = p_list[number - 1]
 }
 
-document.querySelector("#download").addEventListener('mouseover', () => {
+document.querySelector("#download > .button").addEventListener('mouseover', () => {
     document.querySelector("figure").style.opacity = "80%"
 })
 
-document.querySelector("#download").addEventListener('mouseout', () => {
+document.querySelector("#download > .button").addEventListener('mouseout', () => {
     document.querySelector("figure").style.opacity = "10%"
-})
-
-document.querySelector("#points").addEventListener('mouseover', () => {
-    document.querySelector("#points > .net-x").style.height = "700px"
-    document.querySelector("#points > .net-y").style.width = "900px"
-})
-
-document.querySelector("#points").addEventListener('mouseout', () => {
-    document.querySelector("#points > .net-x").style.height = "0px"
-    document.querySelector("#points > .net-y").style.width = "0px"
 })
